6c4a815f0037c7507b26136a0f42b51ec624f2e3
Spencer Amann <amanns@mail.gvsu.edu>
2018-03-01 18:31:42 -0500

    inital commit of files

---
+++ src/main/java/com/droidwolf/lunchpal/lunchpalservice/LunchPalServiceApplication.java
@@ -0,0 +1,12 @@
+package com.droidwolf.lunchpal.lunchpalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchPalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchPalServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/droidwolf/lunchpal/lunchpalservice/LunchPalServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.droidwolf.lunchpal.lunchpalservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LunchPalServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
